57592ff76a1dcca96b9a54cbb0c69bbb3da19f2e
Kedmanee <63070012@kmitl.ac.th>
2024-03-19 00:45:24 +0700

    feat: ass navigate in bottom nav

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // import { Login } from "./pages/Login";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
@@ -8,9 +8,12 @@ import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
 
 export const BottomNav = () => {
   const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
   return (
     <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
       <BottomNavigation
@@ -23,18 +26,22 @@ export const BottomNav = () => {
         <BottomNavigationAction
           label={<span className="kanit-400 bottomNavLabel">หากลุ่ม</span>}
           icon={<RestoreIcon />}
+          onClick={() => navigate("/find-party")}
         />
         <BottomNavigationAction
           label={<span className="kanit-400 bottomNavLabel">สังคมของฉัน</span>}
           icon={<FavoriteIcon />}
+          onClick={() => navigate("/")}
         />
         <BottomNavigationAction
           label={<span className="kanit-400 bottomNavLabel">แชท</span>}
           icon={<LocationOnIcon />}
+          onClick={() => navigate("/find-party")}
         />
         <BottomNavigationAction
           label={<span className="kanit-400 bottomNavLabel">โปรไฟล์</span>}
           icon={<LocationOnIcon />}
+          onClick={() => navigate("/find-party")}
         />
       </BottomNavigation>
     </Paper>
